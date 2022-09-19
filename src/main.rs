c28ea9a0ffcc3f5003f622fa7d6b0727db572923
Rob Whitby <rob.whitby@springer.com>
2022-09-19 16:05:05 +0100

    add linter

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+mod linter;
 mod manifest;
 
+use crate::linter::Linter;
 use clap::Parser;
 use manifest::Manifest;
 use std::error::Error;
 use std::fs::read_to_string;
 use std::path::PathBuf;
+use std::process;
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -21,9 +24,26 @@ struct Args {
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
     let raw_manifest = read_to_string(&args.path)?;
-    let manifest = Manifest::from_yaml(&raw_manifest)?;
-    let yaml = manifest.to_yaml()?;
 
+    let manifest = Manifest::from_yaml(&raw_manifest)?;
+
+    let linter = Linter::new();
+    let lint_results = linter.lint(&manifest);
+
+    for s in &lint_results.errors {
+        eprintln!("  [error] {}", s)
+    }
+
+    lint_results
+        .warnings
+        .iter()
+        .for_each(|s| eprintln!("[warning] {}", s));
+
+    if lint_results.has_errors() {
+        process::exit(1)
+    }
+
+    let yaml = manifest.to_yaml()?;
     println!("{}", yaml);
 
     Ok(())
