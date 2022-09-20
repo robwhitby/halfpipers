3a78b639bb6af7be8e6bab03bc9b9d5290a357c1
Rob Whitby <rob.whitby@springer.com>
2022-09-20 16:43:33 +0100

    linter tests

--- src/lint_rules.rs
+++ src/lint_rules.rs
@@ -17,9 +17,52 @@ pub fn team_must_not_contain_spaces(manifest: &Manifest) -> Option<Issue> {
 }
 
 pub fn pipeline_should_be_lowercase(manifest: &Manifest) -> Option<Issue> {
-    if manifest.pipeline == manifest.pipeline.to_lowercase() {
+    if manifest.pipeline != manifest.pipeline.to_lowercase() {
         Some(Issue::Warning("pipeline should be lowercase".to_string()))
     } else {
         None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn empty_manifest() -> Manifest {
+        Manifest {
+            pipeline: "".to_string(),
+            team: "".to_string(),
+            tasks: vec![],
+        }
+    }
+
+    #[test]
+    fn team_name() {
+        let bad = Manifest {
+            team: "team name".to_string(),
+            ..empty_manifest()
+        };
+        assert!(matches!(team_must_not_contain_spaces(&bad), Some(Issue::Error(..))));
+
+        let good = Manifest {
+            team: "team-name".to_string(),
+            ..empty_manifest()
+        };
+        assert_eq!(team_must_not_contain_spaces(&good), None);
+    }
+
+    #[test]
+    fn pipeline_name() {
+        let bad = Manifest {
+            pipeline: "Pipeline Name".to_string(),
+            ..empty_manifest()
+        };
+        assert!(matches!(pipeline_should_be_lowercase(&bad), Some(Issue::Warning(..))));
+
+        let good = Manifest {
+            pipeline: "pipeline name".to_string(),
+            ..empty_manifest()
+        };
+        assert_eq!(pipeline_should_be_lowercase(&good), None);
     }
 }
